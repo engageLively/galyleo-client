278ff7383434ad225220eff961ccecf6e78730cd
Rick McGeer <rick@mcgeer.com>
2022-03-13 23:06:44 -0700

    minor bugs fixed

--- galyleo/galyleo_server_framework.py
+++ galyleo/galyleo_server_framework.py
@@ -28,82 +28,83 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from os import lseek
 from flask import Flask, request, abort
 from galyleo.galyleo_exceptions import InvalidDataException
 from galyleo.galyleo_table_server import GalyleoDataServer, check_valid_spec
 from json import loads, dumps
 
-
-'''
-A framework to easily and quickly implement a web server which serves tables according to 
-the Galyleo URL protocol.  This implements the URL methods get_filtered_rows, get_all_values,
-and get_numeric_spec.  It parses the arguments, checking for errors, takes the
-table argument, looks up the appropriate GalyleoDataServer to serve for that table, and
-then calls the method on that server to serve the request.  If no exception is thrown,
-returns a 200 with the result as a JSON structure, and if an exception is thrown, returns
-a 400 with an approrpriate error message.
-'''
-app = Flask(__name__)
-table_servers = {}
-
-def add_table_server(table_name, galyleo_data_server):
+class GalyleoServerFramework:
     '''
-    Register a GalyleoDataServer to server data for a specific table name.  
+    A framework to easily and quickly implement a web server which serves tables according to 
+    the Galyleo URL protocol.  This implements the URL methods get_filtered_rows, get_all_values,
+    and get_numeric_spec.  It parses the arguments, checking for errors, takes the
+    table argument, looks up the appropriate GalyleoDataServer to serve for that table, and
+    then calls the method on that server to serve the request.  If no exception is thrown,
+    returns a 200 with the result as a JSON structure, and if an exception is thrown, returns
+    a 400 with an approrpriate error message.
     '''
-    table_servers[table_name] = galyleo_data_server
-
-def _get_table_server(table_name):
-    try:
-        server = table_servers[table_name]
-    except KeyError:
-        abort(400, f'No handler defined for table {table_name}')
-
+    def __init__(self, app):
+        app.add_url_rule('/get_filtered_rows', view_func = self.get_filtered_rows, methods=['POST'])
+        app.add_url_rule('/get_numeric_spec', view_func = self.get_numeric_spec, methods=['GET'])
+        app.add_url_rule('/get_all_values', view_func = self.get_all_values, methods=['GET'])
+        app.add_url_rule('/hello', view_func = self._hello, methods = ['GET'])
+        self.table_servers = {}
     
-@app.route('/get_filtered_rows', methods=['POST'])
-def get_filtered_rows():
-    parameters = request.get_json()
-    if 'table_name' in parameters:
-        server = _get_table_server(parameters['table_name'])
-        if 'filter_spec' in parameters:
-            try:
-                filter_spec = parameters['filter_spec']
-                check_valid_spec(filter_spec)
-                return server.get_filtered_rows(filter_spec)
-            except InvalidDataException as e:
-                abort(400, e)
+    def _hello(self):
+        return 'hello, world'
+
+    def add_table_server(self, table_name, galyleo_data_server):
+        '''
+        Register a GalyleoDataServer to server data for a specific table name.  
+        '''
+        self.table_servers[table_name] = galyleo_data_server
+
+    def _get_table_server(self, table_name):
+        try:
+            return self.table_servers[table_name]
+        except KeyError:
+            abort(400, f'No handler defined for table {table_name}')
+
+        
+    def get_filtered_rows(self):
+        parameters = request.get_json()
+        if 'table_name' in parameters:
+            server = self._get_table_server(parameters['table_name'])
+            if 'filter_spec' in parameters:
+                try:
+                    filter_spec = parameters['filter_spec']
+                    check_valid_spec(filter_spec)
+                    return server.get_filtered_rows(filter_spec)
+                except InvalidDataException as e:
+                    abort(400, e)
+            else:
+                return dumps(server.get_rows())
         else:
-            return server.get_rows()
-    else:
-        abort(400, 'table_name must be supplied to get_filtered_rows')
+            abort(400, 'table_name must be supplied to get_filtered_rows')
 
-def _check_required_parameters(handle, parameter_set):
-    sent_parameters = set(request.args.keys())
-    missing_parameters = parameter_set - sent_parameters
-    if (len(missing_parameters) > 0):
-        abort(400, f'Missing arguments to {handle}: {missing_parameters}')
-
-@app.route('/get_numeric_spec', methods=['GET'])
-def get_numeric_spec():
-    _check_required_parameters('/get_numeric_spec', {'table_name', 'column_name'})
-    server = _get_table_server(request.args.get('table_name'))
-    try:
-        server.get_numeric_spec(request.args.get('column_name'))
-    except InvalidDataException as e:
-        abort(400, e) 
-
-@app.route('/get_all_values', methods=['GET'])
-def get_all_values():
-    _check_required_parameters('/get_all_values', {'table_name', 'column_name'})
-    server = _get_table_server(request.args.get('table_name'))
-    try:
-        server.get_all_values(request.args.get('column_name'))
-    except InvalidDataException as e:
-        abort(400, e) 
+    def _check_required_parameters(self, handle, parameter_set):
+        sent_parameters = set(request.args.keys())
+        missing_parameters = parameter_set - sent_parameters
+        if (len(missing_parameters) > 0):
+            abort(400, f'Missing arguments to {handle}: {missing_parameters}')
 
 
+    def get_numeric_spec(self):
+        self._check_required_parameters('/get_numeric_spec', {'table_name', 'column_name'})
+        server = self._get_table_server(request.args.get('table_name'))
+        try:
+            return dumps(server.numeric_spec(request.args.get('column_name')))
+        except InvalidDataException as e:
+            abort(400, e) 
 
-
-    
+    def get_all_values(self):
+        self._check_required_parameters('/get_all_values', {'table_name', 'column_name'})
+        server = self._get_table_server(request.args.get('table_name'))
+        try:
+            return dumps(server.all_values(request.args.get('column_name')))
+        except InvalidDataException as e:
+            abort(400, e) 
 
     
     
